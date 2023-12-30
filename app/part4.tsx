0b6d5113fd6bcb3e42fd5e2e035a6c3ff9778fdd
Elozino <ovedhee@gmail.com>
2023-12-30 07:22:15 +0100

    Changed the path of the 'Next' button from '/part2' to '/part4'.

---
+++ app/part4.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const part4 = () => {
+  return (
+    <View>
+      <Text>part4</Text>
+    </View>
+  )
+}
+
+export default part4
+
+const styles = StyleSheet.create({})
